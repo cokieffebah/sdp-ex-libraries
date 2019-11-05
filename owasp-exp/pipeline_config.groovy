d4b3e5a74fc4c02134cbba1d980b15557676eead
Carlos OKieffe <okieffe_carlos@bah.com>
2019-11-05 14:54:51 -0500

    added for library testing

---
+++ owasp-exp/pipeline_config.groovy
@@ -0,0 +1,48 @@
+// turn off repo level Jenkinsfiles 
+allow_scm_jenkinsfile = true
+
+application_environments{
+    Dev
+    Test
+    Prod
+}
+
+keywords{
+    override = true
+    master  =  /^[Mm]aster$/
+    develop =  /^[Dd]evelop(ment|er|)$/
+    hotfix  =  /^[Hh]ot[Ff]ix-/
+    release =  /^[Rr]elease-(\d+.)*\d$/
+    test    =  /^[Tt]est(ing|)$/
+}
+
+libraries{
+    // allow applications to add additional libraries
+    merge = true
+    sdp{
+        images{
+            registry = "https://docker.pkg.github.com"
+            repository = "boozallen/sdp-images"
+            cred = "github"
+        }
+    }
+    aws{
+        /*
+            application repositories should specifier the 
+            identifier key
+        */
+        merge = true
+    }    
+    github{
+        source_type = "github"
+    }
+    sonarqube
+    owasp_dep_check{
+        merge = true
+        cvss_threshold = "11"
+    }
+}
+
+steps{
+    merge = true
+}
