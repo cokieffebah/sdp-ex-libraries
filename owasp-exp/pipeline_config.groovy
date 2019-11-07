b0bb5ff1f72ef455305c3f7e5189328355901f9e
Carlos OKieffe <okieffe_carlos@bah.com>
2019-11-06 20:41:46 -0500

    fixing credentials

--- owasp-exp/pipeline_config.groovy
+++ owasp-exp/pipeline_config.groovy
@@ -23,7 +23,7 @@ libraries{
         images{
             registry = "https://docker.pkg.github.com"
             repository = "boozallen/sdp-images"
-            cred = "cokieffebah"
+            cred = "cokieffebah-github"
         }
     }
     aws{
