84510723c6b8dfde3a640cc2a0d73f2e06ebd842
Carlos OKieffe <o'kieffe_carlos@bah.com>
2020-12-09 16:18:40 -0500

    Update pipeline_config.groovy

--- jte-2/pipeline_config.groovy
+++ jte-2/pipeline_config.groovy
@@ -18,6 +18,15 @@ keywords{
 }
 
 @merge libraries{
+    
+    sdp{
+        images{
+            registry = "https://docker.pkg.github.com"
+            repository = "boozallen/sdp-images"
+            cred = "cokieffebah"
+        }
+    }
+    
     git{
         gitlab
     }
