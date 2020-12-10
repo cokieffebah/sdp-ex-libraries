e09b4d8d6d7da3744f3280d6c66a38251ef3a2cc
Carlos OKieffe <okieffe_carlos@bah.com>
2020-12-10 14:44:44 -0500

    testing template checkout with a config

--- jte-2/pipeline_config.groovy
+++ jte-2/pipeline_config.groovy
@@ -1,3 +1,37 @@
+application_environments{
+    Dev
+    Test
+    Prod
+}
 
+jte{
+  allow_scm_jenkinsfile = true 
+}
 
+keywords{
+    override = true
+    master  =  /^[Mm]aster$/
+    develop =  /^[Dd]evelop(ment|er|)$/
+    hotfix  =  /^[Hh]ot[Ff]ix-/
+    release =  /^[Rr]elease-(\d+.)*\d$/
+    test    =  /^[Tt]est(ing|)$/
+}
 
+@merge libraries{
+    
+    /*sdp{
+        images{
+            registry = "https://docker.pkg.github.com"
+            repository = "boozallen/sdp-images"
+            cred = "cokieffebah"
+        }
+    }*/
+    
+    git{
+        gitlab
+    }
+    owasp_dep_check{
+        merge = true
+        cvss_threshold = "11"
+    }
+}
