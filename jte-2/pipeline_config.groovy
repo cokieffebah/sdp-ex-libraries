450d3b18161bcd740444f93cef97501e5d7c254a
Carlos OKieffe <okieffe_carlos@bah.com>
2020-12-10 14:32:03 -0500

    removed all config and calling checkout scm in template

--- jte-2/pipeline_config.groovy
+++ jte-2/pipeline_config.groovy
@@ -1,13 +1,3 @@
 
 
-@merge libraries{
-    
-    sdp{
-        images{
-            registry = "https://docker.pkg.github.com"
-            repository = "boozallen/sdp-images"
-            cred = "cokieffebah"
-        }
-    }
-    
-}
+
