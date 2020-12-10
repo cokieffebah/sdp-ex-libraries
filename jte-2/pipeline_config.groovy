7da670fab2a4046f683ecb293a65a43521d1d53a
Carlos OKieffe <okieffe_carlos@bah.com>
2020-12-10 14:27:26 -0500

    strip down to libraries/sdp

--- jte-2/pipeline_config.groovy
+++ jte-2/pipeline_config.groovy
@@ -1,21 +1,4 @@
-application_environments{
-    Dev
-    Test
-    Prod
-}
 
-jte{
-  allow_scm_jenkinsfile = true 
-}
-
-keywords{
-    override = true
-    master  =  /^[Mm]aster$/
-    develop =  /^[Dd]evelop(ment|er|)$/
-    hotfix  =  /^[Hh]ot[Ff]ix-/
-    release =  /^[Rr]elease-(\d+.)*\d$/
-    test    =  /^[Tt]est(ing|)$/
-}
 
 @merge libraries{
     
@@ -27,11 +10,4 @@ keywords{
         }
     }
     
-    /*git{
-        gitlab
-    }*/
-    owasp_dep_check{
-        merge = true
-        cvss_threshold = "11"
-    }
 }
